2c83de27610c231b5a712d88c1470c39eb4f5527
Abhijeet Bodhankar (Tata Consultancy Services) <v-abbodh@microsoft.com>
2020-02-28 13:50:42 -0800

    modified regex pattern for handling thread.tacv2 url

--- Source/Microsoft.Teams.Apps.FAQPlusPlus.Configuration/Controllers/HomeController.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus.Configuration/Controllers/HomeController.cs
@@ -229,9 +229,9 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Configuration.Controllers
             // team id regex match
             // for a pattern like https://teams.microsoft.com/l/team/19%3a64c719819fb1412db8a28fd4a30b581a%40thread.tacv2/conversations?groupId=53b4782c-7c98-4449-993a-441870d10af9&tenantId=72f988bf-86f1-41af-91ab-2d7cd011db47
             // regex checks for 19%3a64c719819fb1412db8a28fd4a30b581a%40thread.tacv2
-            var match = Regex.Match(teamIdDeepLink, @"/team\/([\s\S %?]*)\/");
+            var match = Regex.Match(teamIdDeepLink, @"teams.microsoft.com/l/team/(\S+)/");
 
-            if (!match.Success || match.Groups.Count < 2)
+            if (!match.Success)
             {
                 return string.Empty;
             }
