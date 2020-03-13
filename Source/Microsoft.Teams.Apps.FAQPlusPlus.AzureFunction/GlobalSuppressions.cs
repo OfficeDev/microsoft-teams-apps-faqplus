2a46b1ca01d5c67ef448974d17984b48aaa126ee
Lalith Kanala <v-laka@microsoft.com>
2020-03-13 16:46:17 +0000

    Merged PR 427: Cloned repo from Office Dev
    Crosschecked all the files with the Office Dev github repo

--- Source/Microsoft.Teams.Apps.FAQPlusPlus.AzureFunction/GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-﻿// <copyright file="GlobalSuppressions.cs" company="Microsoft">
-// Copyright (c) Microsoft. All rights reserved.
-// </copyright>
-
-// This file is used by Code Analysis to maintain SuppressMessage attributes that are applied to this project.
-// Project-level suppressions either have no target or are given a specific target and scoped to a namespace, type, member, etc.
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Not required to catch specific exception.", Scope = "member", Target = "~M:Microsoft.Teams.Apps.FAQPlusPlus.AzureFunction.PublishFunction.Run(Microsoft.Azure.WebJobs.TimerInfo,Microsoft.Extensions.Logging.ILogger)~System.Threading.Tasks.Task")]
