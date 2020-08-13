330cfa34213b60bd2a4b1e6c6845b088862ede79
JOE3PM <63799848+JOE3PM@users.noreply.github.com>
2020-08-13 09:33:20 -0500

    Update TourCarousel.cs

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/TourCarousel.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/TourCarousel.cs
@@ -23,7 +23,7 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Cards
             {
                 GetCard(Strings.TeamNotificationHeaderText, Strings.TeamNotificationContent, appBaseUri + "/content/Notifications.png"),
                 GetCard(Strings.TeamChatHeaderText, Strings.TeamChatContent, appBaseUri + "/content/Enduserchat.png"),
-                GetCard(Strings.TeamTicketSystemHeaderText, Strings.TeamTicketSystemContent, appBaseUri + "/content/Ticketsystem.png"),
+                GetCard(Strings.TeamTicketSystemHeaderText, Strings.TeamTicketSystemContent, appBaseUri + "/content/icono-ema.png"),
             };
         }
 
@@ -38,7 +38,7 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Cards
             {
                 GetCard(Strings.FunctionCardText1, Strings.FunctionCardText2, appBaseUri + "/content/Askaquestion.png"),
                 GetCard(Strings.AskAnExpertTitleText, Strings.AskAnExpertText2, appBaseUri + "/content/Expertinquiry.png"),
-                GetCard(Strings.ShareFeedbackTitleText, Strings.FeedbackText1, appBaseUri + "/content/Sharefeedback.png"),
+                GetCard(Strings.ShareFeedbackTitleText, Strings.FeedbackText1, appBaseUri + "/content/icono-ema.png"),
             };
         }
 
